8ff8142c588ee473626d27ffe35d16d78b44ff01
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-04-13 17:27:20 +0200

    Assigned magic numbers.

--- src/GameConsole/PlayerCharacter.cs
+++ src/GameConsole/PlayerCharacter.cs
@@ -5,7 +5,7 @@ namespace GameConsole
     public class PlayerCharacter
     {
         public string Name { get; set; }
-        public int DaysSinceLastLogin { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int DaysSinceLastLogin { get; set; } = -1; // Magic number
+        public DateTime DateOfBirth { get; set; } = DateTime.MinValue; // Magic number
     }
 }
